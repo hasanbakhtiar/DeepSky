8b319bfde82d0a4815063320ec2f9fe5a4389335
Hasan <hasan.doctype@gmail.com>
2021-03-25 12:10:06 +0400

    add JavaScript Lesson Part1

---
+++ JSLessons/script.js
@@ -0,0 +1,27 @@
+/*alert("Salam");
+for more
+*/
+
+//for one
+
+// alert("JavaScript");
+// window.alert("JavaScript");
+
+// document.write("JavaScript Lessons <br>");
+// document.writeln("Script Lessons");
+
+// console.log("Salam JavaSript");
+
+
+document.writeln("hi Js");
+debugger;
+document.writeln("hi Js");
+debugger;
+document.writeln("hi Js");
+
+document.writeln("hi Js");
+debugger;
+
+
+
+
