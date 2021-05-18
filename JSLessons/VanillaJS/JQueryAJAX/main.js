4a8f080fe4debca6d9d4fe0989371db7c38a9a9e
Hasan <hasan.doctype@gmail.com>
2021-05-18 12:46:31 +0400

    add React Lesson1

---
+++ JSLessons/VanillaJS/JQueryAJAX/main.js
@@ -0,0 +1,16 @@
+$(function(){
+    $("button").click(function(){
+    //   $("#div1").load("data.html h1");
+    // $.get("demo_test.asp", function(data, status){
+    //     alert("Data: " + data + "\nStatus: " + status);
+    //   });
+    // $.post("data.html",
+    // {
+    //   name: "Donald Duck",
+    //   city: "Duckburg"
+    // },
+    // function(data,status){
+    //   alert("Data: " + data + "\nStatus: " + status);
+    // });
+    });
+  });
